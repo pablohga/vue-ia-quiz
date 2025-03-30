eeffb66e34f20e65a7c05fa44a4bcde9e2e65f12
Pabo Azevedo <pablohga@gmail.com>
2025-03-30 00:51:40 +0000

    `feat: Update Quiz.vue component to display current question and add submit button`

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -3,13 +3,23 @@
     <h2>Quiz Comonente</h2>
 
     <div class="question">
-      <h3>{{ props.questions[1].question }}</h3>
+      <h3>{{ props.questions[currentQuestion].question }}</h3>
     </div>
+
+    <button @click="subnmitAnswer">Send</button>
   </section>
 </template>
 
 <script setup>
+import { ref } from "vue"
+
 const props = defineProps(['questions'])
+const currentQuestion =ref(0)
+
+const subnmitAnswer = () =>{
+  currentQuestion.value += 1
+}
+
 console.log(props.questions)
 </script>
 
