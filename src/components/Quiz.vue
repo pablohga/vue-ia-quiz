430c603315440834f5d32eb81563557bc31d18ce
Pabo Azevedo <pablohga@gmail.com>
2025-03-30 02:26:33 +0100

    `feat: Add shuffled answer buttons to Quiz component`

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -6,15 +6,32 @@
       <h3>{{ props.questions[currentQuestion].question }}</h3>
     </div>
 
+    <div class="answers">
+      <button 
+        class="answer"
+        v-for="answer in shuffleOptions"
+        :key="answer"
+      >
+      {{ answer }}
+    
+    </button>
+    </div>
+
     <button @click="subnmitAnswer">Send</button>
   </section>
 </template>
 
 <script setup>
-import { ref } from "vue"
+import { ref, computed } from "vue"
 
 const props = defineProps(['questions'])
 const currentQuestion =ref(0)
+
+const shuffleOptions = computed(() => {
+  let options = [...props.questions[currentQuestion.value].incorrect_answers]
+  options.splice(Math.round((Math.random() * options.length)) , 0, props.questions[currentQuestion.value].correct_answer)
+  return options
+})
 
 const subnmitAnswer = () =>{
   currentQuestion.value += 1
